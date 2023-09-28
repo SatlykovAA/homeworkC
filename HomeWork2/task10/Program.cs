e9dd0478f19c51b0ec50df916cc8db6c175c3d0c
AzatSatlykov <azatsatlykov@yahoo.com>
2023-09-28 20:42:22 +0500

    123

--- HomeWork2/task10/Program.cs
+++ HomeWork2/task10/Program.cs
@@ -1,2 +1,16 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
+Console.Write("Введите трехзначное число:");
+
+int A = Convert.ToInt32(Console.ReadLine());
+int Length = A.ToString().Length;
+if (Length == 3) 
+    {
+		int B = ( A/10 ) % 10;
+		Console.WriteLine($"вторая цифра этого числа {B}");
+    } 
+else 
+    {
+		Console.WriteLine("ERROR!!!!");
+	}
+    
---
+++ HomeWork2/task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число:");
+int Value = Convert.ToInt32(Console.ReadLine());
+int L = Value.ToString().Length;
+if (L >= 3) 
+{
+	while (Value > 999)
+	{
+		Value = Value / 10;
+	}
+	int result = Value % 10;
+	Console.WriteLine("Третья цифра = " + result);
+} 
+else 
+{
+	Console.WriteLine("Нет третьей цифры");
+}
