e961c8595081959be5c0c5076b6265997218e33f
AzatSatlykov <azatsatlykov@yahoo.com>
2023-10-01 20:25:17 +0500

    4

---
+++ HomeWork3/task20/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Write("Введите координату х точки А:  ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y точки А:  ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z точки А:  ");
+int x3 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату х точки B:  ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y точки B:  ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z точки B:  ");
+int y3 = Convert.ToInt32(Console.ReadLine());
+double result =  Math.Sqrt(Math.Pow(y1 - x1, 2) + Math.Pow(y2 - x2, 2) + Math.Pow(y3 - x3, 2));
+
+Console.WriteLine($"Расстояние между точками = {result}");
---
+++ HomeWork3/task23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Таблица кубов: ");
+for (int i=1;i<=N;i++)
+{
+int k = i*i*i;
+Console.WriteLine($"{k} ");
+}
