eb7eb245c8def5bdf62507e6351e0b59826f2e59
AzatSatlykov <azatsatlykov@yahoo.com>
2023-10-02 23:44:56 +0500

    6

---
+++ HomeWork4/task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+
+}
+int Exponent(int A, int B)
+{
+  int result = 1;
+  for(int i=1; i <= B; i++)
+  {
+    result = result * A;
+  }
+   return result;
+}
+
+int A = ReadNumber("Enter A:  ");
+int B = ReadNumber("Enter B:  ");
+
+int exp = Exponent(A, B);
+Console.WriteLine("Ответ: " + exp);
---
+++ HomeWork4/task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+
+}
+int Sum(int A)
+{
+    int res = 0;
+    while (A >0)
+    {
+        res = res + A%10;
+        A = A / 10;
+    }
+   return res;
+  }
+
+
+
+int A = ReadNumber("Enter A:  ");
+Console.WriteLine($"Сумма цифр в числе {A} =  {Sum(A)}");
+
+
+
